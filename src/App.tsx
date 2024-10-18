754f03979c163fe9d89fcc1204dd13f04a2d8ac1
beke.tamas <beke.tamas@BURG-1E06-19>
2024-10-18 13:36:48 +0200

    ye

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import './app.css'
 import PlayerName from './components/PlayerName/PlayerName'
 import Resource from './components/Resource/Resource'
 function App() {
-const [Megacredit, setMegacreidt] = useState(0)
-const [MegacreditProduction, setMegacreditProduction] = useState(0)
-const handleMegacreditChange=(increment:number)=>{
-    setMegacreidt(Megacredit+increment)
+
+  const loadResource=(resource:string)=>{
+    const saveMegaCredit = localStorage.getItem(resource)
+    return saveMegaCredit ? Number(saveMegaCredit) : 0;
+  
+  }
+
+const [Megacredit, setMegacreidt] = useState(loadResource("megacredit"))
+const [MegacreditProduction, setMegacreditProduction] = useState(loadResource("mproduction"))
+
+const [Steel, setSteel] = useState(loadResource("steel"))
+const [SteelProduction, setSteelProduction] = useState(loadResource("sproduction"))
+
+
+const handleIncrement=(increment:number, resource:number, 
+  setResource:(value:number)=>void)=>{
+    setResource(resource+increment)
 }
-const handleMegacreditProduction=(increment:number)=>{
-    setMegacreditProduction(MegacreditProduction+increment)
-}
+
+
+useEffect(()=>{
+  localStorage.setItem("megacredit", Megacredit.toString());
+  localStorage.setItem("steel", Steel.toString());
+  localStorage.setItem("mproduction", MegacreditProduction.toString())
+  localStorage.setItem("sproduction", SteelProduction.toString())
+},[Megacredit,Steel, MegacreditProduction, SteelProduction]);
 
 const handlSubmit2=(e:FormEvent)=>{
     e.preventDefault()
     setMegacreidt(Megacredit+MegacreditProduction)
+    setSteel(Steel+SteelProduction);
 }
 
   return (
@@ -23,9 +42,16 @@ const handlSubmit2=(e:FormEvent)=>{
         <Resource 
         name='Megacredit' 
         amount={Megacredit} 
-        onchange={handleMegacreditChange}
+        onchange={(increment:number) => handleIncrement(increment, Megacredit, setMegacreidt)}
         production={MegacreditProduction}
-        onchangeProductivity={handleMegacreditProduction}
+        onchangeProductivity={(increment:number) => handleIncrement(increment, MegacreditProduction, setMegacreditProduction)}
+        ></Resource>
+        <Resource 
+        name='Steel' 
+        amount={Steel} 
+        onchange={(increment:number)=>handleIncrement(increment, Steel, setSteel)}
+        production={SteelProduction}
+        onchangeProductivity={(increment:number)=>handleIncrement(increment, SteelProduction, setSteelProduction)}
         ></Resource>
        <button type='submit' className='NextRound'>Next Round</button>
     </form>
